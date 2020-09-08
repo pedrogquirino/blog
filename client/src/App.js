bed5e2e7f84575e4a30c1b7cd593ed845cd7b034
pedrogquirino <pedrogquirino@gmail.com>
2020-09-08 15:58:34 -0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
+
+export default () => {
+    return <div className="container">
+        <h1>Post create</h1>
+        <PostCreate />
+        <hr/>
+        <h1>Posts</h1>
+        <PostList />
+    </div>;
+
+};
