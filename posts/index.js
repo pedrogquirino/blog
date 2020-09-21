9c0ea964e050193e7a68a449ec9d67b99d9cb166
pedrogquirino <pedrogquirino@gmail.com>
2020-09-21 14:34:56 -0300

    Adjusting and including ingress-depl file

--- posts/index.js
+++ posts/index.js
@@ -12,11 +12,10 @@ const posts = {};
 
 
 app.get('/posts', (req, res) => {
-
     res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
 
     const { title } = req.body;
